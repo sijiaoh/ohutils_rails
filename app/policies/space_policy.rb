84923d56b46869098a65bbc8d2c48701aa4f5b18
Sijia Wang <sijiaoh@outlook.com>
2022-01-13 07:45:33 +0900

    Fix policy throw error.

--- app/policies/space_policy.rb
+++ app/policies/space_policy.rb
@@ -1,6 +1,6 @@
 class SpacePolicy < ApplicationPolicy
   def index?
-    user.is_admin?
+    user&.is_admin?
   end
 
   def show?
@@ -12,10 +12,10 @@ class SpacePolicy < ApplicationPolicy
   end
 
   def update?
-    user.is_admin?
+    user&.is_admin?
   end
 
   def destroy?
-    user.is_admin?
+    user&.is_admin?
   end
 end
