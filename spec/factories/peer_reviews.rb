22a5acb2482f3ce3bb48aa3588c6a37708d98a7e
Sijia Wang <sijiaoh@outlook.com>
2022-01-31 18:06:55 +0900

    Format factories.

--- spec/factories/peer_reviews.rb
+++ spec/factories/peer_reviews.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :peer_review do
     title { Faker::Book.title }
+
     space
   end
 end
--- spec/factories/student_profiles.rb
+++ spec/factories/student_profiles.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :student_profile do
     student_number { Faker::Number.number(digits: 5).to_s }
+
     user
   end
 end
